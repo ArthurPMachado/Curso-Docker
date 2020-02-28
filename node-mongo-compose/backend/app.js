1ed69590a447f39bb0edaa20d728e77efe88648f
Arthur Pereira Machado <machadoparthur1@gmail.com>
2020-02-28 20:45:49 -0300

    Creating server in node

---
+++ node-mongo-compose/backend/app.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const restful = require('node-restful');
+const server = express();
+const mongoose = restful.mongoose;
+
+// Database
+mongoose.Promise = global.Promise;
+mongoose.connect('mongodb://db/mydb');
+
+// Teste
+server.get('/', (request, response, next) => response.send('Backend'));
+
+// Iniciar servidor
+server.listen(3000);
